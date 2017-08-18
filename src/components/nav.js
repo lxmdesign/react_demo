024749afcdc4b4938798255effad9ae459480898
cexoso <285516193@qq.com>
2017-08-18 21:44:50 +0800

    eslint warning

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,17 +3,8 @@ import {
     NavLink
 } from 'react-router-dom'
 class Nav extends Component {
-    state = {
-        activeLiIndex: 0
-    }
     navs = ["旅游介绍", "边赛信息", "主赛结果"]
-    liClick = (activeLiIndex) => {
-        this.setState({
-            activeLiIndex
-        })
-    }
     render() {
-        const { activeLiIndex } = this.state;
         return (
             <ul className="nav">
                 {this.navs.map((value, key) =>
