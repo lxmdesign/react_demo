ad8971f3a1b03b8c1e20e58f25c1501c88a6ed87
xiongjie <xiongj01@mysoft.com.cn>
2017-08-19 14:32:11 +0800

    add dependencies

--- src/react/head.js
+++ src/react/head.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Attr from "./head/attr";//最上边属性栏
 import Title from "./head/title";//标题栏
 import Message from "./head/message";//标题栏
 import Nav from "./head/nav";//导航栏
